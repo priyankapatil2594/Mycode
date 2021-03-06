b2e3631d4d823b0fe6adb5934ba61572bc987a83
Priyankapatil2594 <patilpriya2594@gmail.com>
2021-03-06 11:24:30 +0530

    My initial commit

---
+++ information.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class information {
+	public static void main(String[] args) {
+		System.out.println("Name- Priyanka Patil");
+		System.out.println("DOB- 25-04-1994");
+		System.out.println("Bloud group-B+");
+		System.out.println("Contact no- 7448175770");
+		
+	}
+
+}
---
+++ simpleprogramm.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class simpleprogramm {
+	public static void main(String[] args) {
+		System.out.println("12 months");
+		String [] abc= new String[5];
+		
+		
+	}
+
+}
